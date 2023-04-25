f33eae90faed3d14bb5646c1d1dab63e12c97060
Дима <82971106+GovnocodedByChapo@users.noreply.github.com>
2023-04-25 21:31:55 +0300

    Add files via upload

--- client/lib/dolbaeb/net.lua
+++ client/lib/dolbaeb/net.lua
@@ -3,6 +3,7 @@ local snet = require('snet');
 local SNetClient, bstream = snet.client('127.0.0.1', 11321), snet.bstream;
 
 
+
 Net = {
     json = json,
     snet = snet,
@@ -13,7 +14,7 @@ Net = {
     server = {
         ip = '127.0.0.1',
         port = 11321
-    }
+    },
 }
 
 ---@alias BS_INT8 any
@@ -36,48 +37,79 @@ Net.Packet = {
     LeaveRoom = 5,
     LeaveRoomResponse = 6,
     getRooms = 7,
-    roomsList = 8
+    roomsList = 8,
+    RoomUpdate = 10,
+    RequestRoomUpdate = 11,
+    ThrowCard = 12,
+    Ready = 13,
+    ChatMessage = 14
 }
 
 Net.PacketStruct = {
     [Net.Packet.JSON] = { 
-        { 'BS_INT16', 'len' },
-        { 'BS_STRING', 'jsonData' }
+        { BS_INT16, 'len' },
+        { BS_STRING, 'jsonData' }
     },
     [Net.Packet.CreateRoom] = {
-        { 'BS_INT16', 'usernameLen' },
-        { 'BS_STRING', 'username' },
+        { BS_INT16, 'usernameLen' },
+        { BS_STRING, 'username' },
     },
     [Net.Packet.CreateRoomResponse] = {
-        { 'BS_BOOLEAN', 'status' },
-        { 'BS_UINT16', 'roomId' }
+        { BS_BOOLEAN, 'status' },
+        { BS_UINT16, 'roomId' }
     },
     [Net.Packet.JoinRoom] = {
-        { 'BS_INT16', 'usernameLen' },
-        { 'BS_STRING', 'username' },
-        { 'BS_UINT16', 'roomId' }
+        { BS_INT16, 'usernameLen' },
+        { BS_STRING, 'username' },
+        { BS_UINT16, 'roomId' }
     },
     [Net.Packet.JoinRoomResponse] = {
-        { 'BS_BOOLEAN', 'status' },
-        { 'BS_INT16', 'messageLen' },
-        { 'BS_STRING', 'message' },
-        { 'BS_INT16', 'roomLen' },
-        { 'BS_STRING', 'room' }
+        { BS_BOOLEAN, 'status' },
+        { BS_INT16, 'messageLen' },
+        { BS_STRING, 'message' },
+        { BS_INT16, 'roomLen' },
+        { BS_STRING, 'room' }
     },
-    getRooms = {},
-    roomsList = {
-        { 'BS_INT16', 'jsonLen' },
-        { 'BS_STRING', 'json' }
-    }
+    [Net.Packet.getRooms] = {},
+    [Net.Packet.roomsList] = {
+        { BS_INT16, 'jsonLen' },
+        { BS_STRING, 'json' }
+    },
+    [Net.Packet.RoomUpdate] = {
+        { BS_INT16, 'jsonLen' },
+        { BS_STRING, 'json' }
+    },
+    [Net.Packet.RequestRoomUpdate] = {
+        { BS_INT16, 'roomId' }
+    },
+    [Net.Packet.ThrowCard] = {
+        { BS_INT16, 'roomId' },
+        { BS_INT16, 'slotId' },
+        { BS_STRING, 'card', 3 }
+    },
+    [Net.Packet.Ready] = {
+        { BS_INT16, 'roomId' },
+        { BS_INT16, 'usernameLen' },
+        { BS_STRING, 'username' },
+    },
+    [Net.Packet.ChatMessage] = {
+        { BS_INT16, 'messageLen' },
+        { BS_STRING, 'message' },
+    },
 }
 
 function Net.readPacket(id, bs)
-    local struct = Net.PacketStruct[id]
+    local previousValue, struct = 0, Net.PacketStruct[id]
     local data = {}
     for index, item in ipairs(struct) do
-        data[item[2]] = bs:read(item[1], item[1] == 'BS_STRING' and bs:read(struct[index-1]) or nil)
+        local valueType, name = item[1], item[2]
+        local value = bs:read(item[1], valueType == BS_STRING and item[3] or previousValue) 
+        data[name] = value
+        previousValue = value
+        print('read', index, value)
+        --data[item[2]] = bs:read(item[1], item[1] == 'BS_STRING' and bs:read(struct[index-1]) or nil)
     end
-    return data
+    return data, json.encode(data)
 end
 
 function Net.writePacket(id, bs)
@@ -89,13 +121,7 @@ function Net.writePacket(id, bs)
     return bs
 end
 
-function Net.setupPacketHandler()
-    
-end
 
-function Net.loop()
-    SNetClient:process();
-end
 
 function Net.sendJSON(id, data)
     local J = json.encode(data)
@@ -106,25 +132,21 @@ function Net.sendJSON(id, data)
 end
 
 ---@param id Net.Packet
+---@param data table
 function Net.send(id, data)
-
+    sampAddChatMessage('123', -1)
+    local struct = Net.PacketStruct[id]
+    assert(struct, 'Struct for this packet not found')
+    local bs = bstream.new()
+    for index, item in ipairs(struct) do
+        bs:write(item[1], data[item[2]])
+    end
+    print(SNetClient.connected)
+    SNetClient:send(id, bs, SNET_SYSTEM_PRIORITY)
 end
 
-function Net.room.create()
-
+function Net.loop()
+    SNetClient:process()
 end
-
-function Net.room.join()
-
-end
-
-function Net.room.leave()
-
-end
-
-
-
-
-
 
 return Net
--- client/lib/dolbaeb/resource.lua
+++ client/lib/dolbaeb/resource.lua
@@ -1,78 +1,28 @@
-Resource = {}
-
-Resource.card = {
-    d = {
-        [1] = { img = nil, base85 = ''},
-        [2] = { img = nil, base85 = ''},
-        [3] = { img = nil, base85 = ''},
-        [4] = { img = nil, base85 = ''},
-        [5] = { img = nil, base85 = ''},
-        [6] = { img = nil, base85 = ''},
-        [7] = { img = nil, base85 = ''},
-        [8] = { img = nil, base85 = ''},
-        [9] = { img = nil, base85 = ''},
-        [10] = { img = nil, base85 = ''},
-        [11] = { img = nil, base85 = ''},
-        [12] = { img = nil, base85 = ''},
-        [13] = { img = nil, base85 = ''},
-        [14] = { img = nil, base85 = ''}
-    },
-    h = {
-        [1] = { img = nil, base85 = ''},
-        [2] = { img = nil, base85 = ''},
-        [3] = { img = nil, base85 = ''},
-        [4] = { img = nil, base85 = ''},
-        [5] = { img = nil, base85 = ''},
-        [6] = { img = nil, base85 = ''},
-        [7] = { img = nil, base85 = ''},
-        [8] = { img = nil, base85 = ''},
-        [9] = { img = nil, base85 = ''},
-        [10] = { img = nil, base85 = ''},
-        [11] = { img = nil, base85 = ''},
-        [12] = { img = nil, base85 = ''},
-        [13] = { img = nil, base85 = ''},
-        [14] = { img = nil, base85 = ''}
-    },
-    s = {
-        [1] = { img = nil, base85 = ''},
-        [2] = { img = nil, base85 = ''},
-        [3] = { img = nil, base85 = ''},
-        [4] = { img = nil, base85 = ''},
-        [5] = { img = nil, base85 = ''},
-        [6] = { img = nil, base85 = ''},
-        [7] = { img = nil, base85 = ''},
-        [8] = { img = nil, base85 = ''},
-        [9] = { img = nil, base85 = ''},
-        [10] = { img = nil, base85 = ''},
-        [11] = { img = nil, base85 = ''},
-        [12] = { img = nil, base85 = ''},
-        [13] = { img = nil, base85 = ''},
-        [14] = { img = nil, base85 = ''}
-    },
-    c = {
-        [1] = { img = nil, base85 = ''},
-        [2] = { img = nil, base85 = ''},
-        [3] = { img = nil, base85 = ''},
-        [4] = { img = nil, base85 = ''},
-        [5] = { img = nil, base85 = ''},
-        [6] = { img = nil, base85 = ''},
-        [7] = { img = nil, base85 = ''},
-        [8] = { img = nil, base85 = ''},
-        [9] = { img = nil, base85 = ''},
-        [10] = { img = nil, base85 = ''},
-        [11] = { img = nil, base85 = ''},
-        [12] = { img = nil, base85 = ''},
-        [13] = { img = nil, base85 = ''},
-        [14] = { img = nil, base85 = ''}
-    }
+Resource = { 
+    img = nil
 }
+local imgui = require('mimgui')
+local oneCard = imgui.ImVec2(1 / 9, 1 / 4)
 
-function Resource.loadCards()
-    for type, list in ipairs(Resource) do
-        for index, data in pairs(list) do
-            Resource[type][index].img = 'createfrom(data.base85)'
-        end
-    end
+function Resource.init()
+    Resource.img = imgui.CreateTextureFromFile(getWorkingDirectory()..'\\lib\\dolbaeb\\cards.png')
+end
+
+function Resource.getCardUV(code)
+    local cardType, cardNumber = code:match('(%a)(%d+)')
+    local lines = {
+        h = 1, -- Hearts (Черви / Сердца)
+        s = 2, -- Diamonds (Бубы / Алмазы)
+        d = 3, -- Clubs (Трефы / Клубы)
+        c = 4  -- Spades (Пики / Лопаты)
+    }
+    cardType = lines[tonumber(cardType) or 1] or 1
+    cardNumber = (tonumber(cardNumber) or 1) - 6
+    return imgui.ImVec2(oneCard.x * cardNumber, oneCard.y * cardType - oneCard.y), imgui.ImVec2(oneCard.x * cardNumber + oneCard.x, oneCard.y * cardType)
+end
+
+function Resource.card(code, size, ...)
+    return imgui.ImageButton(Resource.img, size, imgui.ImVec2(0, 0), Resource.getCardUV(code), ...)
 end
 
 return Resource
